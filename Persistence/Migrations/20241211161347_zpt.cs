b505a498835d72c0e75e762841c764b93f51387b
Mthinh <minhthinh9229@gmail.com>
2024-12-11 23:16:48 +0700

    newdb

--- Persistence/Migrations/20241211161347_zpt.cs
+++ Persistence/Migrations/20241211161347_zpt.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Persistence.Migrations
 {
     /// <inheritdoc />
-    public partial class abc : Migration
+    public partial class zpt : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
