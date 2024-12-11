586a9abac0caa48543a1e92ff7f4a580ea134116
Mthinh <minhthinh9229@gmail.com>
2024-12-12 01:01:36 +0700

    12/12

--- Hotel_App/Data/LoaiPhong.cs
+++ Hotel_App/Data/LoaiPhong.cs
@@ -2,23 +2,23 @@
 
 namespace Hotel_App.Data
 {
-    public class LoaiPhong
-    {
-        [Key]
-        public int MaLoaiPhong { get; set; }
-        [Required(ErrorMessage = "Tên loại phòng không được để trống.")]
-        public string TenLoaiPhong { get; set; }
+	public class LoaiPhong
+	{
+		[Key]
+		public int MaLoaiPhong { get; set; }
+		[Required(ErrorMessage = "Tên loại phòng không được để trống.")]
+		public string TenLoaiPhong { get; set; }
 		[Required(ErrorMessage = "Số lượng phòng không được để trống.")]
 		[RegularExpression(@"^[1-9]\d*$", ErrorMessage = "Số lượng phòng phải là số nguyên lớn hơn 0.")]
 		public int SoLuongPhong { get; set; }
 		[Required(ErrorMessage = "Mô tả không được để trống.")]
 		public string Mota { get; set; }
-        [Required(ErrorMessage = "Giá phòng không được để trống.")]
-        [DataType(DataType.Currency)]
+		[Required(ErrorMessage = "Giá phòng không được để trống.")]
+		[DataType(DataType.Currency)]
 
-		[RegularExpression(@"^[1-9]\d{4,}$", ErrorMessage = "Giá phòng phải là số nguyên ít nhất là 10000.")]
+		[Range(100000, (double)decimal.MaxValue, ErrorMessage = "Giá phòng phải là số nguyên ít nhất là 100000.")]
 		public decimal Gia { get; set; }
-        public LoaiPhong() { }
+		public LoaiPhong() { }
 
 		public int TrangThai { get; set; } = 1;
 
