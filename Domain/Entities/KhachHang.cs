a9acf9c05c75aeb057828a0b86877d8f2abb496a
Mthinh <minhthinh9229@gmail.com>
2024-12-11 22:55:30 +0700

    11/12

--- Domain/Entities/KhachHang.cs
+++ Domain/Entities/KhachHang.cs
@@ -3,22 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities
 {
-    public class KhachHang
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int MaKhachHang { get; set; }
-        public string HoTen { get; set; }
-        
-        public string SoDienThoai { get; set; }
+	public class KhachHang
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int MaKhachHang { get; set; }
+		public string HoTen { get; set; }
 
-        [EmailAddress(ErrorMessage = "Email không đúng định dạng.")]
-        public string Email { get; set; }
-        public DateTime? NgaySinh { get; set; }
-        public string CCCD { get; set; }
-        public string STK { get; set; }
-        public int TrangThai { get; set; }
+		public string SoDienThoai { get; set; }
 
-        public KhachHang() { }
-    }
+		[EmailAddress(ErrorMessage = "Email không đúng định dạng.")]
+		public string Email { get; set; }
+		public DateTime? NgaySinh { get; set; }
+		public string CCCD { get; set; }
+		public string STK { get; set; }
+		public int TrangThai { get; set; }
+
+		public KhachHang() { }
+	}
 }
