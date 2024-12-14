333ec879f81c7db1d5b1437077c9aa6945ebe7ec
Duc <ADMIN@MSI>
2024-12-14 17:48:15 +0700

    bills completed

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,23 +1,10 @@
 ﻿using Application.Features.Commands;
-using MediatR;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Application.Interfaces;
-using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Context;
 using Application;
